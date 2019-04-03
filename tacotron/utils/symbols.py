07b4daca147b7125f0aec8a93277914977485f0b
whfh3900 <48546917+whfh3900@users.noreply.github.com>
2019-04-03 15:54:19 +0900

    Add files via upload

---
+++ tacotron/utils/symbols.py
@@ -0,0 +1,19 @@
+'''
+Defines the set of symbols used in text input to the model.
+
+The default is a set of ASCII characters that works well for English or text that has been run
+through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details.
+'''
+from . import cmudict
+from .korean import ALL_SYMBOLS_1
+
+_pad        = '_'
+_eos        = '~'
+_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'\"(),-.:;? '
+
+# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
+#_arpabet = ['@' + s for s in cmudict.valid_symbols]
+
+# Export all symbols:
+eng_symbols = [_pad, _eos] + list(_characters) #+ _arpabet
+kor_symbols = ALL_SYMBOLS_1
---
+++ train_and_shutdown.sh
@@ -0,0 +1,2 @@
+python train.py --model='Tacotron-2' --checkpoint_interval=1000 --eval_interval=1000
+sudo shutdown -h now
